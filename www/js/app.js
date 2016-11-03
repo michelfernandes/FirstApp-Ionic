67c388616171d593f0bc2d8e94cb53a189a1fcf1
Michel Mentz Fernandes <michel.fernandes@cwi.com.br>
2016-11-03 15:29:12 -0200

    Initial commit including the project

---
+++ www/js/app.js
@@ -0,0 +1,109 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+var db = null;
+angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform, $cordovaSQLite) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+    db = $cordovaSQLite.openDB({ name: 'my.db', location:'default' });
+    $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS people (id integer primary key, firstname text, lastname text)");
+    $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS subject (id integer primary key, name text, description text, status integer)");
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.quotes', {
+    url: '/quotes',
+    views:{
+      'tab-quotes':{
+        templateUrl: 'templates/tab-quotes.html',
+        controller: 'QuotesCtrl'
+      }
+    }
+  })
+
+  .state('tab.database',{
+    url: '/database',
+    views:{
+      'tab-database':{
+        templateUrl: 'templates/tab-database.html',
+        controller: 'DatabaseCtrl'
+      }
+    }
+  })
+
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
+
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,108 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $ionicPopup) {
+
+  $scope.addSubject = function() {
+    var formTemplate = '<label>name:</label><input type="text" ng-model="data.name">'+
+    '<label>description:</label><input type="text" ng-model="data.description">'+
+    '<div class="button-bar">'+
+    '<ion-radio style="width:33.5%" ng-model="choice" ng-value="1"></ion-radio>'+
+    '<ion-radio style="width:33.5%" ng-model="choice" ng-value="2"></ion-radio>'+
+    '<ion-radio style="width:33.5%" ng-model="choice" ng-value="3"></ion-radio>'+
+    '</div>';
+    $scope.data = {};
+    var myPopup = $ionicPopup.show({
+      template: formTemplate,
+      title: 'Enter Wi-Fi Password',
+      subTitle: 'Please use normal things',
+      scope: $scope,
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: '<b>Save</b>',
+          type: 'button-positive',
+          onTap: function(e) {
+            if (!$scope.data.wifi) {
+              //don't allow the user to close unless he enters wifi password
+              e.preventDefault();
+            } else {
+              return $scope.data.wifi;
+            }
+          }
+        }
+      ]
+    });
+  };
+})
+
+.controller('QuotesCtrl', function($scope, $http) {
+
+  $scope.quote = {};
+
+  $http.get('http://quotes.rest/qod.json')
+    .then(
+      function(response){
+        console.log(response);
+        $scope.quote.body = response.data.contents.quotes[0].quote;
+        $scope.quote.author = response.data.contents.quotes[0].author;
+        $scope.quote.date = response.data.contents.quotes[0].date;
+      },
+      function(error){
+        alert('Error: ' + error);
+      }
+    );
+
+})
+
+.controller('DatabaseCtrl', function($scope, $cordovaSQLite){
+
+  $scope.msg = 'controller';
+
+  $scope.insert = function(firstname, lastname) {
+        var query = "INSERT INTO people (firstname, lastname) VALUES (?,?)";
+        $cordovaSQLite.execute(db, query, [firstname, lastname]).then(function(res) {
+            alert("INSERT ID -> " + res.insertId);
+        }, function (err) {
+            alert(err);
+        });
+    }
+
+  $scope.select = function(lastname) {
+        var query = "SELECT firstname, lastname FROM people WHERE lastname = ?";
+        $cordovaSQLite.execute(db, query, [lastname]).then(function(res) {
+            if(res.rows.length > 0) {
+                alert("SELECTED -> " + res.rows.item(0).firstname + " " + res.rows.item(0).lastname);
+            } else {
+                alert("No results found");
+            }
+        }, function (err) {
+            alert(err);
+        });
+    }
+
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
