0db49e2f4816c3adbd9da19e2c8b5f733b4de9a3
Michel Mentz Fernandes <michel.fernandes@cwi.com.br>
2016-11-11 10:51:18 -0200

    Alterar controle de settings de volume

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -173,6 +173,7 @@ angular.module('starter.controllers', [])
 
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
-    enableFriends: true
+    enableFriends: true,
+    volume: 50
   };
 });
