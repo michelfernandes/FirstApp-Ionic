5a8719f676405f58e2d2670f9559ae62f42334a6
Michel Mentz Fernandes <michel.fernandes@cwi.com.br>
2016-11-17 11:28:47 -0200

    Adicinar arquivos de mock do angular para testes de controller

--- www/spec/DashCtrlSpec.js
+++ www/spec/DashCtrlSpec.js
@@ -1,6 +1,27 @@
 'use strict';
+
 describe('Test', function() {
   it('testing', function() {
     expect(1).toBe(1);
+  });
+});
+
+describe('DashCtrl', function() {
+  var scope, $location, createController;
+
+    beforeEach(inject(function ($controller, _$location_) {
+        $location = _$location_;
+
+        createController = function() {
+            return $controller('DashCtrl', {
+                '$scope': scope
+            });
+        };
+    }));
+
+  describe('scope showInformation', function() {
+    it('Show information in alert popup', function() {
+      //var controller = createController();
+    });
   });
 });
